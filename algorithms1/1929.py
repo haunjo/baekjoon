fd912f09e6c36434cb3ad31d97928a5145af6f4e
haunjo <johaun12@naver.com>
2023-07-04 19:57:21 +0900

    2023-07-04

--- algorithms1/1929.py
+++ algorithms1/1929.py
@@ -1,13 +1,21 @@
+# 소수 구하기, S3
+
 M, N = map(int, input().split())
 
-numbers = list(range(M,N+1))
-if M == 1:
-    del numbers[0]
+numbers = [True] * (N+1)
+numbers[0] = False
+numbers[1] = False
 
-for i in range(2,N):
-    for k in numbers:
-        if k%i == 0 and k != i:
-            numbers.remove(k)
+for i in range(2, int(N**0.5 +1)):
+    k = 2
+    while i*k <= N:
+        numbers[i*k] = False
+        k = k + 1
 
-for i in numbers:
-    print(i)
+for j in range(M,N+1):
+    if numbers[j]:
+        print(j)
+            
+
+
+
